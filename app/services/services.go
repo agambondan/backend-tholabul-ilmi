45d9ae5bdf8da1baf7431a3d4efb9cb6933969a6
firmanagam <agamgans89@gmail.com>
2026-05-02 23:51:22 +0700

    fix: make services init nil-safe

--- app/services/services.go
+++ app/services/services.go
@@ -5,35 +5,38 @@ import (
 )
 
 type Services struct {
-	User           UserService
-	Ayah           AyahService
-	Surah          SurahService
-	Juz            JuzService
-	Book           BookService
-	Theme          ThemeService
-	Chapter        ChapterService
-	Hadith         HadithService
-	Bookmark       BookmarkService
+	User            UserService
+	Ayah            AyahService
+	Surah           SurahService
+	Juz             JuzService
+	Book            BookService
+	Theme           ThemeService
+	Chapter         ChapterService
+	Hadith          HadithService
+	Bookmark        BookmarkService
 	ReadingProgress ReadingProgressService
-	Hafalan        HafalanService
-	Streak         StreakService
-	Search         SearchService
+	Hafalan         HafalanService
+	Streak          StreakService
+	Search          SearchService
 }
 
 func NewServices(repo *repository.Repositories) *Services {
+	if repo == nil {
+		return &Services{}
+	}
 	return &Services{
-		User:           NewUserService(repo.User),
-		Ayah:           NewAyahService(repo.Ayah),
-		Surah:          NewSurahService(repo.Surah),
-		Juz:            NewJuzService(repo.Juz),
-		Book:           NewBookService(repo.Book),
-		Theme:          NewThemeService(repo.Theme),
-		Chapter:        NewChapterService(repo.Chapter),
-		Hadith:         NewHadithService(repo.Hadith),
-		Bookmark:       NewBookmarkService(repo.Bookmark, repo.Ayah, repo.Hadith),
+		User:            NewUserService(repo.User),
+		Ayah:            NewAyahService(repo.Ayah),
+		Surah:           NewSurahService(repo.Surah),
+		Juz:             NewJuzService(repo.Juz),
+		Book:            NewBookService(repo.Book),
+		Theme:           NewThemeService(repo.Theme),
+		Chapter:         NewChapterService(repo.Chapter),
+		Hadith:          NewHadithService(repo.Hadith),
+		Bookmark:        NewBookmarkService(repo.Bookmark, repo.Ayah, repo.Hadith),
 		ReadingProgress: NewReadingProgressService(repo.ReadingProgress, repo.UserActivity),
-		Hafalan:        NewHafalanService(repo.Hafalan),
-		Streak:         NewStreakService(repo.UserActivity),
-		Search:         NewSearchService(repo.Search),
+		Hafalan:         NewHafalanService(repo.Hafalan),
+		Streak:          NewStreakService(repo.UserActivity),
+		Search:          NewSearchService(repo.Search),
 	}
 }
